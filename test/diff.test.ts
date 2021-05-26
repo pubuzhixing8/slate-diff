5b01deb869bc07e83fb111733082830a85ac890e
pubuzhixing8 <pubuzhixing@gmail.com>
2021-05-26 10:38:24 +0800

    chore(test): add test

--- test/diff.test.ts
+++ test/diff.test.ts
@@ -1,30 +1,19 @@
 import { slateDiff } from "../diff";
+import * as basic from './basic';
+import * as addBlock from './add-block';
+import * as addTextAndAddMark from './add-text-and-add-mark';
 
 
 describe('diff', () => {
     test('basic', () => {
-        const doc1 = [
-            {
-                type: 'paragraph',
-                children: [
-                    {text: 'adsl'}
-                ]
-            }
-        ];
-        const doc2 = [
-            {
-                type: 'heding-two',
-                children: [
-                    {text: 'adsl'}
-                ]
-            },
-            {
-                type: 'heding-one',
-                children: [
-                    {text: 'adsladsl'}
-                ]
-            }
-        ];
-        expect(slateDiff(doc1, doc2)).toBe([]);
+        expect(slateDiff(basic.doc1, basic.doc2)).toBe([]);
+    });
+
+    test('add block', () => {
+        expect(slateDiff(addBlock.doc1, addBlock.doc2)).toBe([]);
+    });
+
+    test('add-text-and-add-mark', () => {
+        expect(slateDiff(addTextAndAddMark.doc1, addTextAndAddMark.doc2)).toBe([]);
     });
 });
